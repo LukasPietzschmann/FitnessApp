e5ca9b31c882ae2ce7b10ec6c47360b86434683d
Vincent Ugrai <ugrai.vincent@gmail.com>
2021-04-22 17:40:51 +0200

    front page updated

--- frontend/src/components/FrontPage/FrontPage.js
+++ frontend/src/components/FrontPage/FrontPage.js
@@ -7,7 +7,7 @@ function FrontPage({ className }) {
 			<h1 className='mt-5'>Titel</h1>
 			<button className='frontEndBtn btn-dark' onClick={() => window.location='/test'}>first button</button>
 			<button className='frontEndBtn btn-dark' onClick={() => window.location='/test'}>second button</button>
-			<button className='frontEndBtn btn-dark' onClick={() => window.location='/test'}>third button</button>
+			<button className='frontEndBtn btn-dark' onClick={() => window.location = '/test'}>third button</button>
 		</div>
 	);
 }
