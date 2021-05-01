df3fc56836c0b7d930c186a1c08669a4314dd139
Lukas Pietzschmann <lukaspie00@gmail.com>
2021-05-01 18:22:41 +0200

    Frontend: added useCookie() hook

---
+++ frontend/src/hooks/useCookie.js
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+function useCookie(cookieName) {
+	const [cookies, setCookies] = useState([]);
+	const [_, refresh] = useState(false);
+
+	useEffect(() => {
+		setCookies(document.cookie.split(';').map((cookie) => {
+			let elems = cookie.split('=');
+			return { name: elems[0], value: elems[1], args: elems.slice(2) }
+		}));
+	}, [_]);
+
+	let cookie = cookies.find(elem => elem.name === cookieName);
+
+	return [cookie, () => {
+		if (cookie == undefined)
+			return;
+		document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
+		refresh();
+	}];
+}
+
+export default useCookie;
