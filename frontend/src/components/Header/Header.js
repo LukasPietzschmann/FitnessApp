51362375ec55f87e82676c995118d0dbd1debe6e
Lukas Pietzschmann <lukaspie00@gmail.com>
2021-05-01 14:18:05 +0200

    Frontend: Updated Header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,10 +4,14 @@ import profilPic from '../../image/sample-profile.png';
 
 function Header({ className }) {
 	return (
-		<nav className={` navbar navbar-expand-lg shadow ${className}`}>
-			<img className='d-flex rounded-circle nav-link ml-3 mt-3' src={logo} height='85rem' alt='Profile picture' onClick={() => window.location = '/'} />
-			<h1 className='ml-3'>Name</h1>
-			<img className='d-flex rounded-circle nav-link disabled ml-auto mr-3 mt-3' src={profilPic} height='85rem' alt='Profile picture'/>
+		<nav className={`navbar navbar-light shadow ${className}`}>
+			<a className='navbar-brand' href='/'>
+				<img className='align-center mr-2' src={logo} height='40rem' alt='Logo' />
+				App-Name
+			</a>
+			<a href='/profile'>
+				<img className='rounded-circle' src={profilPic} height='50rem' alt='Profile picture' />
+			</a>
 		</nav>
 
 	);
