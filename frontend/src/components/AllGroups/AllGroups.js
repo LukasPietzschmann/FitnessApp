8ece4387dd4982421e75f9770ff86bb4b07e8020
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-05-12 09:37:42 +0200

    Frontend: Added Card to join a Group

--- frontend/src/components/AllGroups/AllGroups.js
+++ frontend/src/components/AllGroups/AllGroups.js
@@ -2,12 +2,17 @@ import { useEffect, useState } from 'react';
 
 import { axiosInstance } from '../../constants';
 import useUser from '../../hooks/useUser';
+import Modal from '../Modal/Modal';
 import GroupCard from './GroupCard.js';
 
 
 function AllGroups({ className }) {
 	const [token, uid, logout] = useUser();
 	const [groups, setGroups] = useState([]);
+	const [j, join] = useState(false);
+	const [a, add] = useState(false);
+	const [joinLink, setJoinLink] = useState('');
+	const [error, setError] = useState('');
 
 	useEffect(() => {
 		axiosInstance.get(`/user/${uid}/groups`, { headers: { Token: token } })
@@ -17,8 +22,37 @@ function AllGroups({ className }) {
 
 	return (
 		<div className={`${className}`}>
+			<Modal showModal={j} showModalHook={r => { join(r); setError(''); setJoinLink('')}}>
+				<div className='text-center text-danger mb-2'>{error}</div>
+				<form>
+					<label>Invitation Link</label>
+					<input className='form-control' type='url' placeholder='Put your Invitation Link here...' value={joinLink} onChange={e => setJoinLink(e.target.value)} />
+				</form>
+				<button className='btn btn-success mt-3' onClick={() => {
+					if (joinLink === '') {
+						setError('No Link was given');
+						return;
+					}
+					const match = /(?:http[s]?:\/\/)?[a-zA-Z0-9.]*(?::[0-9]*)?\/groups\/([0-9]{10})\/join/i.exec(joinLink);
+					if (!match){
+						setError('Invalid Invitation Link');
+						return;
+					}
+					const gid = match[1];
+					axiosInstance.post(`/group/${gid}/${uid}`, null, { headers: { Token: token } })
+						.then(res => window.location.href = `/groups/${gid}`)
+						.catch(err => setError(err.response));
+
+				}}>Join Group</button>
+				<button className='btn btn-outline-danger mt-3 float-right' onClick={() => { join(false); setError(''); setJoinLink('')}} >Cancel</button>
+			</Modal>
+			<Modal showModal={a} showModalHook={add}>
+				Add
+			</Modal>
 			<div className='d-inline-flex mt-3 ml-3'>
-				{groups.map(({ gname, img, _id }, i) => <GroupCard className='shadow-lg ml-1' key={i} title={gname} target={`/groups/${_id}`} img={img} />)}
+				{groups.map(({ gname, img, _id }, i) => <GroupCard className='shadow-lg m-2' key={i} title={gname} target={`/groups/${_id}`} img={img} />)}
+				<GroupCard className='shadow-lg m-2' key='join' title='Join Group' onClick={() => join(true)} />
+				<GroupCard className='shadow-lg m-2' key='add' title='Add Group' onClick={() => add(true)} />
 			</div>
 		</div>
 	);
--- frontend/src/components/AllGroups/GroupCard.js
+++ frontend/src/components/AllGroups/GroupCard.js
@@ -1,8 +1,8 @@
 import '../ButtonBehavior.css';
 
-function GroupCard({ className, title, target, img }) {
+function GroupCard({ className, title, target, onClick, img }) {
 	return (
-		<div className={`card ${className}`} onClick={() => window.location.href = target} style={{ width: '38vh' }}>
+		<div className={`card ${className}`} style={{ width: '38vh' }} onClick={target ? () => window.location.href = target : onClick}>
 			<img className='card-img-top' src={img} alt={title} style={{ height: '35vh', objectFit: 'cover' }} />
 			<div className='card-body' style={{backgroundColor: '#1995d1'}}>
 				<h5 className={` text-light text-center ${title}`}>{title}</h5>
