e2817b18229fecb5d9cb1030aea06d63edc1eb28
Blackyoshi <johannesschenker762@gmail.com>
2021-05-16 16:21:27 +0200

    New Optic for the "join group for link" screen

--- frontend/src/components/SingleGroup/GroupInvitation.js
+++ frontend/src/components/SingleGroup/GroupInvitation.js
@@ -6,11 +6,15 @@ import useUser from '../../hooks/useUser';
 function GroupInvitation({ className, match }) {
 	const [token, uid, logout] = useUser();
 	const [groupName, setName] = useState('');
-	//const [groupPicture, setName] = useState('');
+	const [groupPicture, setImg] = useState('');
 
 	useEffect(() => {
 		axiosInstance.get(`/group/${match.params.group_id}/name`)
 			.then(({ data }) => setName(data))
+			.catch(err => console.error(err));
+
+		axiosInstance.get(`/group/${match.params.group_id}/img`)
+			.then(({ data }) => setImg(data))
 			.catch(err => console.error(err));
 	}, []);
 
@@ -22,13 +26,23 @@ function GroupInvitation({ className, match }) {
 			</ div>
 		);
 	return (
-		<div className={className}>
-			<button className='btn btn-success' onClick={() => {
+		<div className='d-flex justify-content-center align-items-center' style={{ height: '70vh' }}>
+			<div class="text-center">
+			</div>
+			<div className={`card hover-card ${className}`} onClick={() => {
 				axiosInstance.post(`/group/${match.params.group_id}/${uid}`, null, { headers: { Token: token } })
 					.then(res => window.location.href = `/groups/${match.params.group_id}`)
+
 					.catch(err => console.error(err.response));
-			}}> Join Group <b>{groupName}</b></button >
+			}}
+
+				style={{ height: '38vh', width: '40vh' }}>
+				<img className='card-img-top' src={groupPicture} alt={groupName} style={{ height: '30vh', objectFit: 'cover' }} />
+				<div className='card-body' style={{ backgroundColor: '#28a745' }}>
+					<h5 className={` text-lSight text-center ${groupName}`}> Join Group <b>{groupName}</b></h5>
+				</div>
 			</div>
+		</div>
 	)
 }
 
