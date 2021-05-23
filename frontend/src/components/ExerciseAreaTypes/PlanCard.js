1a6ec142e8015498d332106e67304db3135096d6
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-05-23 11:54:48 +0200

    Frontend: A Plan can now be added to a user

--- frontend/src/components/ExerciseAreaTypes/PlanCard.js
+++ frontend/src/components/ExerciseAreaTypes/PlanCard.js
@@ -1,14 +1,27 @@
+import { axiosInstance } from '../../constants';
+import useUser from '../../hooks/useUser';
 import '../Cards/ButtonBehavior.css';
 
 function PlanCard({ className, name, units, id }) {
+	const [token, uid, logout] = useUser();
+
 	return (
-		<div className={`card hover-card p-3 ${className}`}>
-			<div className='row'>
-				<h1 className='col'>{name}</h1>
+		<div className={`card ${className}`}>
+			<div className='card-body'>
+				<div className='row'>
+					<h1 className='col'>{name}</h1>
+				</div>
+				<div className='row justify-content-start'>
+					{units.map(({ name, rep }, i) =>
+						<div className='col-auto' key={i}>{name} ({rep} Wiederholung{rep > 1 ? 'en' : ''})</div>)}
+				</div>
 			</div>
-			<div className='row justify-content-start'>
-				{units.map(({ name, rep }, i) =>
-					<div className='col-auto' key={i}>{name} ({rep} Wiederholung{rep > 1 ? 'en' : ''})</div>)}
+			<div className='card-footer btn' onClick={() => {
+				axiosInstance.post(`/user/${uid}/plans`, { "pid": id }, { headers: { Token: token } })
+					.then(res => window.location.href = '/')
+					.catch(err => console.error(err));
+			}}>
+				Add to Profile
 			</div>
 		</div>
 	);
