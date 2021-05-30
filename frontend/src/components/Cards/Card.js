2425b8452202bdc6ba495c77e860c6eb81e2f696
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-05-30 19:42:40 +0200

    Frontend: Updated Card

--- frontend/src/components/Cards/Card.js
+++ frontend/src/components/Cards/Card.js
@@ -3,8 +3,8 @@ import './ButtonBehavior.css';
 function Card({ className, img = '', onClick, children }) {
 	return (
 		<div className={`card hover-card ${className}`} style={{ width: '18rem' }} onClick={onClick}>
-			<img className='card-img-top' src={img} style={{height: '200px', objectFit: 'cover'}}/>
-			<div className='card-body'>
+			<img className='card-img-top position-relative' src={img} style={{ height: '200px', objectFit: 'cover', zIndex: 1, overflow: 'hidden' }}/>
+			<div className='card-body bg-white position-relative' style={{zIndex: 100}}>
 				{children}
 			</div>
 		</div>
