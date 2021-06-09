940b6d7d028726bdfd2337b6eebb70b76667a26d
Lukas Pietzschmann <lukas.pietzschmann@outlook.de>
2021-06-09 11:01:38 +0200

    Frontend: Added utility method to convert a file to a base64 string

---
+++ frontend/src/tools/getBase64ImageData.js
@@ -0,0 +1,19 @@
+function getBase64ImageData(url) {
+	var img = new Image();
+	img.setAttribute('crossOrigin', 'anonymous');
+	let p = new Promise(resolve => {
+		img.onload = function () {
+			var canvas = document.createElement("canvas");
+			canvas.width = this.width;
+			canvas.height = this.height;
+			var ctx = canvas.getContext("2d");
+			ctx.drawImage(this, 0, 0);
+			var dataURL = canvas.toDataURL("image/jpg");
+			resolve(dataURL.replace(/^data:image\/(png|jpg);base64,/, ""));
+		}
+	})
+	img.src = url;
+	return p;
+}
+
+export default getBase64ImageData;
