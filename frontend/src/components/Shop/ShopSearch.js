1b27c54f66e7f6bc5d0cacab0692685f5884b923
Gremmling <ugrai.vincent@gmail.com>
2021-06-23 15:07:39 +0200

    frontend: Shop updated

--- frontend/src/components/Shop/ShopSearch.js
+++ frontend/src/components/Shop/ShopSearch.js
@@ -5,9 +5,10 @@ import Card from './ShopSearchCard';
 function ShopSearch({ className }) {
 	const [items, setItems] = useState(null);
 	const [searchQ, setSearchQ] = useState('');
+	const [invalidSearchQ, setInvalidSearchQ] = useState('');
 
 	return (
-		<div>
+		<div className={`container ${className}`}>
 			<div className='row justify-content-center mt-5'>
 				<h2>Search your Product</h2>
 			</div>
@@ -17,29 +18,26 @@ function ShopSearch({ className }) {
 						<input type='search' className='form-control rounded' placeholder='Search' aria-label='Search' aria-describedby='search-addon' value={searchQ} onChange={e => setSearchQ(e.target.value)}/>
 						<button type='button' className='btn btn-primary' onClick={() => {
 							axiosInstance.get(`shoppingsearch/${searchQ}`)
-								.then(({ data }) => setItems(data))
+								.then(({ data }) => {
+									if (data.length === 0)
+										setInvalidSearchQ(searchQ);
+									setItems(data);
+								})
 								.catch(err => console.error(err));
 						}}>Search</button>
 					</div>
 				</div>
 			</div>
 			<div className='row justify-content-center mt-5'>
-				<div className='col-auto'>
-					{items && (items.length > 0 ? items.map(({ name, priceInEuro, href }, i) => { return (
-						<Card className='shadow-lg m-3' key={i}>
-							<div className='row mt-3 justify-content-center'>
-								<div className='col ml-3 align-self-start'>
-									<h5 className='card-title'>{name}</h5>
-									<a className='card-text' href={href}>Test</a>
-								</div>
-								<div className='col-auto mr-3 align-self-center'>
-									<p className='card-text'>{priceInEuro}</p>
-								</div>
-							</div>
-
-						</Card>
-					)}) : <h3>Item not Available</h3>)}
-				</div>
+				{items && (items.length > 0 ? items.map(({ name, priceInEuro, href }, i) => { return (
+					<Card className='shadow-lg m-3' key={i}>
+						<div className='p-4'>
+							<h5 className='card-title d-inline'>{name}</h5>
+							<div className='card-text d-inline float-right'>{priceInEuro.toFixed(2)}€</div>
+						</div>
+						<div className='card-footer text-center' onClick={() => window.open(href)} style={{cursor: 'pointer'}}>Go to Amazon</div>
+					</Card>
+				)}) : <h3 className='text-danger'>Item {invalidSearchQ} not Available</h3>)}
 			</div>
 		</div>
 	);
--- frontend/src/components/Shop/ShopSearchCard.js
+++ frontend/src/components/Shop/ShopSearchCard.js
@@ -1,10 +1,7 @@
-import '../Cards/ButtonBehavior.css';
-
-
 function ShopSearchCard({ className, onClick, children }) {
 	return (
-			<div className={`card hover-card ${className}`} style={{ width: '18rem' }} onClick={onClick}>
-				<div className='card-title bg-white position-relative' style={{zIndex: 100}}>
+			<div className={`card ${className}`} style={{ width: '18rem' }} onClick={onClick}>
+				<div className='bg-white position-relative' style={{zIndex: 100}}>
 					{children}
 				</div>
 			</div>
